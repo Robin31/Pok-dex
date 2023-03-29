2a467a4fa9a853b1035855b12c93d3baf0a058ea
Robin31 <robindenis@hotmail.fr>
2023-03-29 10:57:29 +0200

    react quete 4

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,20 +1,10 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
-function PokemonCard() {
-  const pokemon = pokemonList[1]
+function PokemonCard(props) {
+  
   return (
     <figure>
-      {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>) : (<p>???</p>)}
-          <figcaption>{pokemon.name}</figcaption>
+      {props.pokemon.imgSrc ? (<img src={props.pokemon.imgSrc} alt={props.pokemon.name}/>) : (<p>???</p>)}
+          <figcaption>{props.pokemon.name}</figcaption>
     </figure>
   )
 }
